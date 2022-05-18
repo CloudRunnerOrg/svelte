de1edffd20d5215ac8e9142dbc6c0453d1b4b478
Nevil Macwan <93511721+nevilm-lt@users.noreply.github.com>
2022-05-18 15:47:17 +0530

    Update test.ts

--- test/test.ts
+++ test/test.ts
@@ -13,7 +13,7 @@ const solo_folders = test_folders.filter(folder => /\.solo/.test(folder));
 
 if (solo_folders.length) {
 	if (process.env.CI) {
-		throw new Error('Forgot to remove `.solo` from test');
+		throw new Error('Forgot to remove  `.solo` from test');
 	}
 	solo_folders.forEach(name => require('./' + name));
 } else {
